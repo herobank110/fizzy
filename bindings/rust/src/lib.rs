a2cf53fc2680fc5be34fb795932354069adf6a3a
Alex Beregszaszi <alex@rtfs.hu>
2020-09-21 22:48:33 +0100

    rust: add Module and parse

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -4,8 +4,31 @@
 
 mod sys;
 
+use std::ptr::NonNull;
+
 pub fn validate<T: AsRef<[u8]>>(input: T) -> bool {
     unsafe { sys::fizzy_validate(input.as_ref().as_ptr(), input.as_ref().len()) }
+}
+
+pub struct Module {
+    ptr: *mut sys::FizzyModule,
+}
+
+impl Drop for Module {
+    fn drop(&mut self) {
+        // This is null if we took ownership with instantiate
+        if !self.ptr.is_null() {
+            unsafe { sys::fizzy_free_module(self.ptr) }
+        }
+    }
+}
+
+pub fn parse<T: AsRef<[u8]>>(input: &T) -> Option<Module> {
+    let ptr = unsafe { sys::fizzy_parse(input.as_ref().as_ptr(), input.as_ref().len()) };
+    if ptr.is_null() {
+        return None;
+    }
+    Some(Module { ptr: ptr })
 }
 
 #[cfg(test)]
@@ -28,5 +51,11 @@ mod tests {
             validate(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]),
             false
         );
+    }
+
+    #[test]
+    fn parse_wasm() {
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00]).is_some());
+        assert!(parse(&[0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x01]).is_none());
     }
 }
