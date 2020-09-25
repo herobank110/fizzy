726b6e79cbb1821390e6923a3466cfe20c70d2ce
Andrei Maiboroda <andrei@ethereum.org>
2020-09-25 17:51:02 +0200

    capi: Add functions for parse, instantiate, execute

--- lib/fizzy/capi.cpp
+++ lib/fizzy/capi.cpp
@@ -2,10 +2,24 @@
 // Copyright 2020 The Fizzy Authors.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "cxx20/bit.hpp"
+#include "execute.hpp"
+#include "instantiate.hpp"
 #include "parser.hpp"
 #include <fizzy/fizzy.h>
+#include <memory>
 
 extern "C" {
+struct FizzyModule
+{
+    fizzy::Module module;
+};
+
+struct FizzyInstance
+{
+    fizzy::Instance* instance;
+};
+
 bool fizzy_validate(const uint8_t* wasm_binary, size_t wasm_binary_size)
 {
     try
@@ -17,5 +31,89 @@ bool fizzy_validate(const uint8_t* wasm_binary, size_t wasm_binary_size)
     {
         return false;
     }
+}
+
+FizzyModule* fizzy_parse(const uint8_t* wasm_binary, size_t wasm_binary_size)
+{
+    try
+    {
+        auto cmodule = std::make_unique<FizzyModule>();
+        cmodule->module = fizzy::parse({wasm_binary, wasm_binary_size});
+        return cmodule.release();
+    }
+    catch (...)
+    {
+        return nullptr;
+    }
+}
+
+void fizzy_free_module(FizzyModule* module)
+{
+    delete module;
+}
+
+FizzyInstance* fizzy_instantiate(FizzyModule* module,
+    const FizzyExternalFunction* imported_functions, uint32_t imported_functions_size)
+{
+    try
+    {
+        // Convert fizzy_external_function to fizzy::ExternalFunction
+        std::vector<fizzy::ExternalFunction> functions(imported_functions_size);
+        for (size_t imported_func_idx = 0; imported_func_idx < imported_functions_size;
+             ++imported_func_idx)
+        {
+            auto func = [cfunc = imported_functions[imported_func_idx]](fizzy::Instance& instance,
+                            fizzy::span<const fizzy::Value> args,
+                            int depth) -> fizzy::ExecutionResult {
+                FizzyInstance cinstance{&instance};
+                const auto cres = cfunc.function(cfunc.context, &cinstance,
+                    reinterpret_cast<const FizzyValue*>(args.data()),
+                    static_cast<uint32_t>(args.size()), depth);
+
+                if (cres.trapped)
+                    return fizzy::Trap;
+                else if (!cres.has_value)
+                    return fizzy::Void;
+                else
+                    return fizzy::bit_cast<fizzy::Value>(cres.value);
+            };
+
+            // TODO get type from input array
+            auto func_type = module->module.imported_function_types[imported_func_idx];
+
+            functions[imported_func_idx] =
+                fizzy::ExternalFunction{std::move(func), std::move(func_type)};
+        }
+
+        auto instance = fizzy::instantiate(std::move(module->module), std::move(functions));
+
+        auto cinstance = std::make_unique<FizzyInstance>();
+        cinstance->instance = instance.release();
+
+        fizzy_free_module(module);
+        return cinstance.release();
+    }
+    catch (...)
+    {
+        fizzy_free_module(module);
+        return nullptr;
+    }
+}
+
+void fizzy_free_instance(FizzyInstance* instance)
+{
+    delete instance->instance;
+    delete instance;
+}
+
+FizzyExecutionResult fizzy_execute(FizzyInstance* instance, uint32_t func_idx,
+    const FizzyValue* cargs, uint32_t args_size, int depth)
+{
+    const auto args = reinterpret_cast<const fizzy::Value*>(cargs);
+
+    const auto result = fizzy::execute(
+        *instance->instance, func_idx, fizzy::span<const fizzy::Value>(args, args_size), depth);
+
+    return {result.trapped, result.has_value, fizzy::bit_cast<FizzyValue>(result.value)};
 }
 }
