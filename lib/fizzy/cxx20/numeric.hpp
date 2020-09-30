54a5740d5fd4ec399f44b75ad2af9bb7af181f09
Alex Beregszaszi <alex@rtfs.hu>
2020-09-30 15:19:51 +0100

    Review

--- lib/fizzy/cxx20/numeric.hpp
+++ lib/fizzy/cxx20/numeric.hpp
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#if __cplusplus > 201703L
+#ifdef __cpp_lib_bitops
 
 #include <bit>
 
@@ -13,7 +13,8 @@ namespace fizzy
 template <class T>
 constexpr int popcount(T x) noexcept
 {
-    return std::popcount.as<T>(x);
+    static_assert(sizeof(x) == sizeof(unsigned int) || sizeof(x) == sizeof(unsigned long long));
+    return std::popcount(x);
 }
 }  // namespace fizzy
 
@@ -25,13 +26,15 @@ namespace fizzy
 {
 constexpr int popcount(uint32_t x) noexcept
 {
+    static_assert(sizeof(x) == sizeof(unsigned int));
     return __builtin_popcount(x);
 }
 
 constexpr int popcount(uint64_t x) noexcept
 {
+    static_assert(sizeof(x) == sizeof(unsigned long long));
     return __builtin_popcountll(x);
 }
 }  // namespace fizzy
 
-#endif /* __cplusplus > 201703L */
+#endif /* __cpp_lib_bitops */
